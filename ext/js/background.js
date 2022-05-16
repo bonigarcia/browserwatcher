a4e2f2b7b346a3ac96fe070352a676de76224a01
Boni Garcia <boni.garcia@uc3m.es>
2022-05-16 17:14:12 +0200

    Include logic for diabling Content-Security-Policy (CSP)

--- ext/js/background.js
+++ ext/js/background.js
@@ -163,3 +163,25 @@ function injectCode(js) {
         chrome.tabs.executeScript(tabs[0].id, { code: js });
     });
 }
+
+// Logic for diabling Content-Security-Policy (CSP)
+
+var onHeadersReceived = function(details) {
+    for (var i = 0; i < details.responseHeaders.length; i++) {
+        if (details.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
+            details.responseHeaders[i].value = '';
+        }
+    }
+
+    return {
+        responseHeaders: details.responseHeaders
+    };
+};
+
+var onHeaderFilter = { urls: ['*://*/*'], types: ['main_frame', 'sub_frame'] };
+
+chrome.webRequest.onHeadersReceived.addListener(
+    onHeadersReceived, onHeaderFilter, ['blocking', 'responseHeaders']
+);
+
+chrome.browsingData.remove({}, { serviceWorkers: true }, function() { });
